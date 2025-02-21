003a5c8d95ef2e0736d0030702f435df3ffc83a8
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-02-21 12:33:12 +0530

    new commit

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ const io = require("socket.io")(server)
 //io --> whole User 
 io.on("connection", (socket) => {
     console.log(`connected ID : ${socket.id}`);
-    socket.emit('connected to server event', (data) => {
-        console.log(`You with ID:${socket.id} connected to server`);
-    });
+
 
     // Example of sending a message to Postman
     socket.on('send message', (message) => {
         console.log(`Message from ${socket.id}: ${message}`);
-        // Here you can add code to send the message to Postman or any other service
+
+        io.emit("chat-message", message )
     });
 });
 
